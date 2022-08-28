58992bee042a82f93e0e6720f538909280062f47
Rafał Gawlak <rafal1463@gmail.com>
2022-08-28 23:26:32 +0200

    styles

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import styles from './Loader.module.scss';
 import { RotatingLines } from 'react-loader-spinner';
 
 export const Loader = () => {
-  const {loader} = styles;
 
   return (
-    <div className={loader}>
+    <div >
       <RotatingLines
         margin="0 auto"
         strokeColor="#3f51b5"
